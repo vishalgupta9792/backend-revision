322a1186db80b460c3e5c7d42c6e3d051db4fe5e
vishalgupta9792 <vishalkgf6264@gmail.com>
2026-05-24 14:44:35 +0530

    Clean up Express server setup

--- Nodejs/server.js
+++ Nodejs/server.js
@@ -1,9 +1,16 @@
-const express = require('express');
+const express = require("express");
 
- const app = express()
+const app = express();
+const PORT = 3000;
 
- app.get("/" , (req,res) => {
-    res.send("Hello World")
- })
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
- app.listen(3000)
+app.get("/about", (req, res) => {
+  res.send("This is about");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
